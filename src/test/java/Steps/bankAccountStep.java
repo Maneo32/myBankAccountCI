3efbc22be8bc78e9b28b708f5668e460dd248b37
Manéo <maneo.dangreaux@uphf.fr>
2023-04-04 09:02:11 +0200

    Changement de fichier myCI.yml

--- src/test/java/Steps/bankAccountStep.java
+++ src/test/java/Steps/bankAccountStep.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en_scouse.An;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,11 +28,30 @@ public class bankAccountStep {
     }
 
 
-    @Then("je devrais voir un message de depot effectue avec succes et le solde {int}")
-    public void jedevraisvoirunmessage(int solde){
+    @Then("Le solde du compte 1 est de 1500")
+    public void jedevraisvoirunmessage(){
         System.out.println("THEN");
         assertEquals(bankManager.getBalance(1), 1500, 0);
     }
 
 
+    @Given("Creation du compte 3")
+    public void compte3cree(){
+        bankManager.create(3);
+    }
+
+    @When("je depose 1000")
+    public void depose1000(){
+        bankManager.deposit(3, 1000);
+    }
+
+    @And("je retire 500")
+    public void retire500(){
+        bankManager.withdraw(3, 500);
+    }
+
+    @Then("Mon solde est a 500")
+    public void solde500(){
+        assertEquals(bankManager.getBalance(3), 499, 0);
+    }
 }
--- src/test/resources/Features/bankAccount.feature
+++ src/test/resources/Features/bankAccount.feature
@@ -1,11 +1,13 @@
 Feature: deposer argent
 
   @Depot
-  Scenario Outline: Deposer avec succes
+  Scenario: Compte 1
     Given Un nouveau compte bancaire 1 est cree avec un solde initial de 1000
     When Je depose 500 dans le compte 1
     Then Le solde du compte 1 est de 1500
-    Examples:
-      | solde | montant | attendue |
-      | 10    | 100     | 110      |
-      | 0    | 2000     | 2000      |
+
+    Scenario: Compte 3
+      Given Creation du compte 3
+      When je depose 1000
+      And je retire 500
+      Then Mon solde est a 500
